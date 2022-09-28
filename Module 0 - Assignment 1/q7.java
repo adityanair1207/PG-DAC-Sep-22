cf2909f423cac0e0736900cfc38edae9199f625d
adityanair1207 <113436487+adityanair1207@users.noreply.github.com>
2022-09-28 15:05:26 +0530

    Add files via upload

---
+++ Module 0 - Assignment 1/q7.java
@@ -0,0 +1,25 @@
+class q7{
+	public static void main(String args[]){
+		int i=1;
+		System.out.println(i);
+		i=i+1;
+		System.out.println(i);
+		i=i+1;
+		System.out.println(i);
+		i=i+1;
+		System.out.println(i);
+		i=i+1;
+		System.out.println(i);
+		i=i+1;
+		System.out.println(i);
+		i=i+1;
+		System.out.println(i);
+		i=i+1;
+		System.out.println(i);
+		i=i+1;
+		System.out.println(i);
+		i=i+1;
+		System.out.println(i);
+		i=i+1;
+	}
+}
